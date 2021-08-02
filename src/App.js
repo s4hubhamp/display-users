df261bdd61e482c7aef08ea5f2b8b72ad30d3295
Shubham <pwrs.0007@gmail.com>
2021-08-02 19:03:39 +0530

    ready to deploy

--- src/App.js
+++ src/App.js
@@ -1,25 +1,90 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+const App = () => {
+  const [pageNumber, setPageNumber] = useState(1);
+  const [users, setUsers] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+
+  useEffect(() => {
+    setIsLoading(true);
+    fetch(`https://reqres.in/api/users?page=${pageNumber}`)
+      .then((res) => res.json())
+      .then((body) => {
+        setIsLoading(false);
+        setUsers(body.data);
+      });
+  }, [pageNumber]);
+
+  const tableHeader = (
+    <thead>
+      <tr>
+        <th scope="col">#</th>
+        <th scope="col">Avatar</th>
+        <th scope="col">First Name</th>
+        <th scope="col">Last Name</th>
+        <th scope="col">Email</th>
+      </tr>
+    </thead>
   );
-}
+
+  const tableBody = (
+    <tbody>
+      {users.map((u, idx) => {
+        return (
+          <tr key={u.id}>
+            <th scope="row">{idx + 1}</th>
+            <td>
+              <img className="avatar" src={u.avatar} alt={u.first_name}></img>
+            </td>
+            <td>{u.first_name}</td>
+            <td>{u.last_name}</td>
+            <td className="email">{u.email}</td>
+          </tr>
+        );
+      })}
+    </tbody>
+  );
+
+  return (
+    <>
+      {isLoading && <div className="h5 mx-auto">Loading...</div>}
+
+      {!isLoading && (
+        <div className="mx-auto user-list">
+          <table className="table table-striped table-bordered">
+            {tableHeader}
+            {tableBody}
+          </table>
+        </div>
+      )}
+
+      {!isLoading && (
+        <div className="mx-auto mt-5">
+          <button
+            className="btn-dark px-3 rounded"
+            onClick={() => {
+              if (pageNumber !== 1) {
+                setPageNumber(1);
+              }
+            }}
+          >
+            1
+          </button>
+          <button
+            className="btn-dark px-3 rounded ms-5"
+            onClick={() => {
+              if (pageNumber !== 2) {
+                setPageNumber(2);
+              }
+            }}
+          >
+            2
+          </button>
+        </div>
+      )}
+    </>
+  );
+};
 
 export default App;
